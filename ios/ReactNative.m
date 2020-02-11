7c26ec22493c641c3022b8eaa9672d78418ce23e
sekizlipenguen <muratdemir3475@gmail.com>
2020-02-11 16:56:57 +0300

    init

---
+++ ios/ReactNative.m
@@ -0,0 +1,14 @@
+#import "ReactNative.h"
+
+
+@implementation ReactNative
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+{
+    // TODO: Implement some actually useful functionality
+    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+}
+
+@end
